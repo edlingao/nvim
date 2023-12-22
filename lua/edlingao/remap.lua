5913393bcf96c3c8fa8442ea99f9fd69544040f1
edlingao <edlingao@edlingao.com>
2023-12-22 13:19:44 -0600

    fixing tab (no body wants tabs)

--- lua/edlingao/remap.lua
+++ lua/edlingao/remap.lua
@@ -1,4 +1,10 @@
 vim.g.mapleader = " "
+
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.smartindent = true
+
 vim.keymap.set("n", "<leader>pv", ":ex<cr>")
 -- vim.keymap.set("n", "C-h", ":ex<cr>")
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>")
