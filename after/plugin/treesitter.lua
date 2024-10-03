15a39a005cd7bc9297225fc2a99f10d2d7743a57
Edwin Linares <sonido244@gmail.com>
2024-10-02 21:33:37 -0600

    Adding treesiter and gopls

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,18 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "vimdoc", "javascript", "typescript", "rust", "go", "c", "lua", "vim", "vimdoc", "query", "html", "gopls" },
+  ensure_installed = {
+    "vimdoc",
+    "javascript",
+    "typescript",
+    "rust",
+    "go",
+    "c",
+    "lua",
+    "vim",
+    "vimdoc",
+    "query",
+    "html",
+  },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
